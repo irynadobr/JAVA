a466cd9f91dc49e8a695445fffa75635859f8317
irynadobr <irenkad@ukr.net>
2021-01-14 23:09:23 +0200

    Lesson 7-stream

--- src/Book.java
+++ src/Book.java
@@ -1,18 +1,12 @@
 import lombok.*;
 
-
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Book {
     private String nameBook;
     private int numberOfPages;
-
-    public Book() {
-    }
-
-    public Book(String nameBook, int numberOfPages) {
-        this.nameBook = nameBook;
-        this.numberOfPages = numberOfPages;
-    }
 
 
     }
--- src/Person.java
+++ src/Person.java
@@ -5,23 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Person {
 
     private String name;
     private LocalDate  birthday;
     private int age;
     private List<Book>books= Arrays.asList();
-
-    public Person() {
-    }
-
-    public Person(String name, LocalDate birthday, int age, List<Book> books) {
-        this.name = name;
-        this.birthday = birthday;
-        this.age = age;
-        this.books = books;
-    }
-
 
 
 }
